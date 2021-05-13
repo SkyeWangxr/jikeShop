fe7cbaeb4ebe8b19f910ad481b1f77a4a3687df6
SkyeWangxr <291782521@qq.com>
2021-05-13 15:30:44 +0800

    初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="container">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		
+	}
+</style>
